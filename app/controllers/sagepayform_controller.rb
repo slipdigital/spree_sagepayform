7e00fdd0b1b0f346edae4b8d9ffcd9f9c13e938f
Douglas Mills <slipdigital@gmail.com>
2013-01-22 16:29:59 +0000

    Updated Version

--- app/controllers/sagepayform_controller.rb
+++ app/controllers/sagepayform_controller.rb
@@ -38,7 +38,7 @@ module Spree
 
 
       # :amount, :currency, :test, :credential2, :credential3, :credential4, :country, :account_name, :transaction_type
-      spf = BillingIntegration::Sagepayform.new()
+      # spf = BillingIntegration::Sagepayform.new()
 
 
       spfh = ActiveMerchant::Billing::Integrations::SagePayForm::Helper.new(@order, "tubchairs123")
